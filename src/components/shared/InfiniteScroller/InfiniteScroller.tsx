e6906cb9afb42529e579b6e460ef226a6582a66d
Adhin <adhin.alifarchan@ist.id>
2025-01-14 16:05:24 +0700

    fix issues

--- src/components/shared/InfiniteScroller/InfiniteScroller.tsx
+++ src/components/shared/InfiniteScroller/InfiniteScroller.tsx
@@ -13,16 +13,16 @@ const InfiniteScroller = (props: { arrays: string[] }) => {
     function addAnimation() {
       scrollers.forEach((scroller) => {
         // add data-animated="true" to every `.scroller` on the page
-        scroller.setAttribute("data-animated", true);
+        scroller.setAttribute("data-animated", "true");
 
         // Make an array from the elements within `.scroller-inner`
-        const scrollerInner = scroller.querySelector(".scroller__inner");
+        const scrollerInner: any = scroller.querySelector(".scroller__inner");
         const scrollerContent = Array.from(scrollerInner.children);
 
         // For each item in the array, clone it
         // add aria-hidden to it
         // add it into the `.scroller-inner`
-        scrollerContent.forEach((item) => {
+        scrollerContent.forEach((item: any) => {
           const duplicatedItem = item.cloneNode(true);
           duplicatedItem.setAttribute("aria-hidden", true);
           scrollerInner.appendChild(duplicatedItem);
